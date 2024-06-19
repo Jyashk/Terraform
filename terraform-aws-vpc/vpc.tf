536f314b5a391f7b3f1e3b8143cc784cf2de1ad3
jupalli yashwanth kumar <yashwanthhny@gmail.com>
2024-06-19 12:50:24 +0530

    subnet assosication

--- terraform-aws-vpc/vpc.tf
+++ terraform-aws-vpc/vpc.tf
@@ -157,15 +157,21 @@ resource "aws_route_table" "database" {
 }
 
 
-resource "aws_route_table_association" "private_subnetassoication" {
+resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidrs)
-  subnet_id      = aws_subnet.private.id[count.index]
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "db_subnetassoication" {
-  count = length(var.public_subnet_cidrs)
-  subnet_id      = aws_subnet.database.id[count.index]
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id      = element(aws_subnet.database[*].id, count.index)
+  route_table_id = aws_route_table.database.id
 }
 
